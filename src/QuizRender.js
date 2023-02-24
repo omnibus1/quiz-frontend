af78049a76d494ae1d48c6d441b31bae59e468ee
apple <appleofeden8686@gmail.com>
2023-02-24 18:16:31 +0100

    woof

--- src/QuizRender.js
+++ src/QuizRender.js
@@ -1,8 +1,11 @@
 import {useEffect, useState} from 'react'
-import Quiz from './Session';
+
 const QuizRender = () => {
-    
+    const [sprawdz,setSprawdz]=useState(false)
+    const [answerDict,setAnswerDict]=useState({})
     const [questionData,setQuestionData]=useState([]);
+    const [currentIndex,setCurrentIndex]=useState(0)
+    const [currentQuestion,setCurrentQuestion]=useState({})
     useEffect(()=>{
         fetch("http://127.0.0.1:8000/api/quiz/"+localStorage.getItem("Id")+"/questions")
         .then(res=>{
@@ -12,28 +15,88 @@ const QuizRender = () => {
             console.log(data)
             
             setQuestionData(data)
-            
             setCurrentQuestion(data[0])
+            
             
         })
     },[])
-    const [currentQuestion,setCurrentQuestion]=useState([])
+    useEffect(() => {
+        if(Object.keys(currentQuestion).length !== 0){
+            let tmpDict={}
+            for(const element of currentQuestion.answers){
+                
+                tmpDict[element.text]=false
+                }
+            
+            setAnswerDict(tmpDict)
+            
+        }
+        
+        
+    }, [currentQuestion]);
+
+    const handleClickSprawdz=async () =>{
+        
+        
+        setSprawdz(true)
+    }
+    
+    const handleClickNext = async () => {
+        setSprawdz(false)
+        if(currentIndex<questionData.length-1){
+            setCurrentQuestion(questionData[currentIndex+1])
+            setCurrentIndex(currentIndex+1)
+        }
+      };
+      const handleClickLast = async () => {
+        setSprawdz(false)
+        if(currentIndex>0){
+            setCurrentQuestion(questionData[currentIndex-1])
+            setCurrentIndex(currentIndex-1)
+        }
+      };
+    
+    const giveColor=(answerElement)=>{
+        if(!sprawdz){
+            return 'white'
+        }
+        else if(answerElement.answer===answerDict[answerElement.text]&&sprawdz){
+            return 'green'
+        }
+        return 'red'
+    }
+    const handleAnwserChange=(e,answerElement)=> {
+        let isChecked=e.target.checked
+        console.log(answerElement.text)
+        let tmp={}
+        Object.assign(tmp,answerDict)
+        console.log(tmp)
+        tmp.setState(answerElement.text,isChecked)
+        console.log(tmp)
+
+
+    }
     return ( 
         <div>
             <h1>{localStorage.getItem("QuizName")}</h1>
             <h1>{localStorage.getItem("Id")}</h1>
-            {questionData.map((question)=>(
-                    <div key={question.question_text}>
-                        <h1>{question.question_text}</h1>
-                        {question.answers.map((questionAnswer)=>(
-                            <div>
-                                <h1>{questionAnswer.text}</h1>
-                                <h1>{questionAnswer.answer.toString()}</h1>
+            <h1>{currentQuestion.question_text}</h1>
+            
+            
+            {currentQuestion.answers&& currentQuestion.answers.map((questionAnswer)=>(
+                            <div key={questionAnswer.text} style={{background:giveColor(questionAnswer)}}>
+                                <input type="checkbox" id={questionAnswer.text} onChange={e=>handleAnwserChange(e,questionAnswer)}/>
+                                <label htmlFor={questionAnswer.text}>{questionAnswer.text}</label>
+                                <h2>   {questionAnswer.answer.toString()}</h2>
+                                
+                                
                             </div>
                             
                         ))}
-                    </div>
-                ))}
+        <h1 onClick={()=>handleClickSprawdz()}>Sprawdz</h1>
+        <h1 onClick={()=>handleClickNext()}>Nastepne</h1>
+        <h1 onClick={()=>handleClickLast()}>Poprzednie</h1> 
+        <h1>{currentIndex+1}/{questionData.length}</h1>
         </div>
      );
 }
